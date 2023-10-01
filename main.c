3bb3a20a559aa2d29868753a36dc2e6a2bc6759c
Aarni Gratseff <aarni.gratseff@gmail.com>
2023-10-02 00:34:07 +0300

    Debris rotation speed

--- main.c
+++ main.c
@@ -146,6 +146,8 @@ fx_t random_debris_x();
 #define NUM_DEBRIS 56
 static fx3_t debris_positions[NUM_DEBRIS];
 static fx3_t debris_rotation_axes[NUM_DEBRIS];
+static fx_t debris_rotations[NUM_DEBRIS];
+static fx_t debris_rotation_speeds[NUM_DEBRIS];
 static fx3_t debris_speeds[NUM_DEBRIS];
 
 static inline void randomize_debris_properties(uint16_t i) {
@@ -156,15 +158,19 @@ static inline void randomize_debris_properties(uint16_t i) {
     fx3_normalize_ip(&r);
     debris_rotation_axes[i] = r;
 
+    debris_rotations[i] = fx_random_one();
+
     switch (i & 3) {
         case 0:
         case 1:
         case 2:
+            debris_rotation_speeds[i] = (fx_random_one() >> 7) + 256;
             debris_speeds[i].x = (fx_random_one() >> 12);
             debris_speeds[i].y = (fx_random_one() >> 12) - (FX_ONE >> 10);
             debris_speeds[i].z = (fx_random_one() >> 12);
             break;
         case 3:
+            debris_rotation_speeds[i] = (fx_random_one() >> 8) + 256;
             debris_speeds[i].x = (fx_random_one() >> 13);
             debris_speeds[i].y = (fx_random_one() >> 13) - (FX_ONE >> 10);
             debris_speeds[i].z = (fx_random_one() >> 13);
@@ -315,6 +321,11 @@ static void update_debris() {
             } else if (debris_positions[i].z >= 32768) {
                 debris_positions[i].z -= 65536;
             }
+        }
+
+        debris_rotations[i] += debris_rotation_speeds[i];
+        if (debris_rotations[i] >= FX_ONE) {
+            debris_rotations[i] -= FX_ONE;
         }
     }
 }
@@ -632,9 +643,12 @@ static void draw_debris(const fx4x3_t* view_matrix) {
     fx_t rotation_angle;
     fx3_t transformed_position;
     const fx3_t* position_iter = debris_positions;
+    const fx_t* rotation_iter = debris_rotations;
     uint16_t i;
     for (i = 0; i < NUM_DEBRIS; ++i) {
         const fx3_t* position = position_iter++;
+        rotation_angle = *rotation_iter++;
+
         fx_transform_point(&transformed_position, view_matrix, position);
         if (transformed_position.z < NEAR_CLIP)
             continue;
@@ -646,7 +660,6 @@ static void draw_debris(const fx4x3_t* view_matrix) {
             transformed_position.y > DEBRIS_BOTTOM_CLIP)
             continue;
 
-        rotation_angle = position->y << 3;
         fx_quat_rotation_axis_angle(&rotation, &debris_rotation_axes[i], rotation_angle);
 
         switch (i & 3) {
